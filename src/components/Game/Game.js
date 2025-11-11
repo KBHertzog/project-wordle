4cfeec7f6edbe279ee46d3fe414ffecb26bdc4a4
Kenton Hertzog <Kenton@Hertzogs.com>
2025-11-10 21:06:56 -0500

    First Two Steps

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+
+import GuessInput from "../GuessInput";
+import GuessHistory from "../GuessHistory/GuessHistory";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +12,22 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guessHistory, setGuessHistory] = React.useState([]);
+
+  return (
+    <>
+      <GuessHistory guesses={guessHistory} />
+      <GuessInput
+        onGuess={(guess) => {
+          if (guessHistory.includes(guess)) {
+            window.alert("You already guessed that word!");
+            return;
+          }
+          setGuessHistory([...guessHistory, guess]);
+        }}
+      />
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function GuessInput({ onGuess }) {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  return (
+    <form
+      class="guess-input-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onGuess(currentGuess);
+        setCurrentGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={currentGuess}
+        onChange={(e) => setCurrentGuess(e.target.value?.toUpperCase() || "")}
+        pattern="\w{5,5}"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
